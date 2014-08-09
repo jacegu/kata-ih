b6f7d756ee46b50bd4d7bd7dfe0a8e38db5ec573
Javier Acero <j4cegu@gmail.com>
2014-08-09 16:37:13 +0200

    Make existing spec more readable and complete

--- spec/users_controller_spec.rb
+++ spec/users_controller_spec.rb
@@ -2,16 +2,22 @@ require 'http_spec_helper'
 require 'users_controller'
 
 describe UsersController do
-  let(:mongo) do
-    Mongo::Connection.new(MONGO_HOST, MONGO_PORT).db(MONGO_DATABASE)
+  let(:accounts) { mongo['accounts'] }
+  let(:mongo) { Mongo::Connection.new(MONGO_HOST, MONGO_PORT).db(MONGO_DATABASE) }
+
+  before { accounts.drop }
+
+  describe 'creating an account' do
+    context 'when the email is not taken' do
+      it 'creates a new account' do
+        post_and_inspect '/account', email: 'yo@email', password: 'foobar'
+        expect(last_response).to be_ok
+        expect(number_of_accounts).to eq(1)
+      end
+    end
   end
 
-  before do
-    mongo['accounts'].drop
-  end
-
-  it 'can post' do
-    post_and_inspect '/account', email: 'yo@email', password: 'foobar'
-    expect(last_response).to be_ok
+  def number_of_accounts
+    accounts.count
   end
 end
