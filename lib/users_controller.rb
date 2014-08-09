92dc077eae4af89be7d7c8488d0687caa17b15f5
Javier Acero <j4cegu@gmail.com>
2014-08-09 16:51:51 +0200

    Extract method with email delivery logic

--- lib/users_controller.rb
+++ lib/users_controller.rb
@@ -29,30 +29,36 @@ class UsersController < Sinatra::Base
         created_at: Time.now,
         last_signed_in_at: nil
       )
-      Pony.mail(
-        charset: 'utf-8',
-        to:      email,
-        from:    'hola@foobar.com <hola@foobar.com>',
-        subject: 'Verifica tu cuenta de foobar',
-        body:    %Q{
-        Verifica tu email y accede a tu cuenta desde https://foobar.com/verification/#{confirmation_token}"
-
-        ¡Muchas gracias!
-        },
-        via: :smtp,
-        via_options: {
-          address:              SMTP_HOST,
-          port:                 SMTP_PORT,
-          enable_starttls_auto: SMTP_USE_TTLS,
-          user_name:            SMPT_USERNAME,
-          password:             SMTP_PASSWORD,
-          authentication:       :plain,
-          domain:               "localhost.localdomain"
-        }
-      )
+      send_verification_email(email, confirmation_token)
     else
       status 409
       json error: "Ya hay un usuario registrado con el email #{email}"
     end
+  end
+
+  private
+
+  def send_verification_email(email, confirmation_token)
+    Pony.mail(
+      charset: 'utf-8',
+      to:      email,
+      from:    'hola@foobar.com <hola@foobar.com>',
+      subject: 'Verifica tu cuenta de foobar',
+      body:    %Q{
+      Verifica tu email y accede a tu cuenta desde https://foobar.com/verification/#{confirmation_token}"
+
+      ¡Muchas gracias!
+      },
+      via: :smtp,
+      via_options: {
+        address:              SMTP_HOST,
+        port:                 SMTP_PORT,
+        enable_starttls_auto: SMTP_USE_TTLS,
+        user_name:            SMPT_USERNAME,
+        password:             SMTP_PASSWORD,
+        authentication:       :plain,
+        domain:               "localhost.localdomain"
+      }
+    )
   end
 end
